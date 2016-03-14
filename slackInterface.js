a3d782e6e7cb90b09c92aad1653236fe82404dea
Carlos Blanco <cblanco@conwet.com>
2016-03-14 20:20:43 +0100

    First sdh slackBot logic

---
+++ slackInterface.js
@@ -0,0 +1,163 @@
+/*
+
+    #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+      This file is part of the Smart Developer Hub Project:
+        http://www.smartdeveloperhub.org/
+      Center for Open Middleware
+            http://www.centeropenmiddleware.com/
+    #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+      Copyright (C) 2015 Center for Open Middleware.
+    #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+      Licensed under the Apache License, Version 2.0 (the "License");
+      you may not use this file except in compliance with the License.
+      You may obtain a copy of the License at
+                http://www.apache.org/licenses/LICENSE-2.0
+      Unless required by applicable law or agreed to in writing, software
+      distributed under the License is distributed on an "AS IS" BASIS,
+      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+      See the License for the specific language governing permissions and
+     limitations under the License.
+    #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+*/
+    // Bootkit or Slack Bots
+    var Botkit = require('botkit');
+    var os = require('os');
+
+    GLOBAL.controller = Botkit.slackbot({
+        debug: false
+    });
+
+    var bot = controller.spawn({
+        token: SLACK_BOT_TOKEN
+    }).startRTM();
+
+
+
+    //var a = sdhBot.getSDHMembers(function(data) {slog.info(data)});
+
+
+    module.exports.setListeners = function setListeners(callback){
+        for (var pattern in sdhBot.knownPatterns) {
+            generateSlackListener (pattern, 'direct_message', sdhBot.knownPatterns[pattern]);
+        }
+        callback();
+    };
+
+    module.exports.getSlackMembers = function getSlackMembers(callback) {
+        bot.api.users.list({}, function(err, res) {
+            if (err) {
+                log.error(err);
+                return;
+            }
+            callback(res.members);
+        })
+    };
+
+    controller.hears(['demo'], 'direct_message', function(bot, message) {
+        log.info("DEMO");
+        bot.reply(message, "All people love demos " + message.user + "...");
+    });
+
+    var generateSlackListener = function generateSlackListener (pattern, type, patternInfo) {
+        pattern = pattern.substring(1, pattern.length-1);
+        var newP = pattern.split("|");
+        controller.hears(newP, type, function(bot, message) {
+            patternInfo.callback(message.user, message, function(coreResponse) {
+                genericSlackCalback(bot, message, pattern, coreResponse);
+            });
+        });
+    };
+
+    var genericSlackCalback = function genericSlackCalback(bot, message, pattern, coreResponse) {
+        bot.reply(message,JSON.stringify(coreResponse));
+    };
+
+    /*// reply to @bot hello
+    controller.on('',function(bot,message) {
+
+      // reply to _message_ by using the _bot_ object
+      bot.reply(message,'I heard you mention me!');
+
+    });
+
+    controller.on('ambient',function(bot,message) {
+
+      // reply to _message_ by using the _bot_ object
+      bot.reply(message,'Ok. ambient!');
+
+    });
+
+    controller.on('message_received',function(bot,message) {
+
+      // reply to _message_ by using the _bot_ object
+      bot.reply(message,'Ok. message_received!');
+
+    });
+    controller.on('user_group_join',function(bot,message) {
+
+      // reply to _message_ by using the _bot_ object
+        log.warn("New user: ");
+        log.debug(message);
+      bot.reply(message,'Ok. user_group_join!');
+
+    });
+    controller.on('user_channel_join',function(bot,message) {
+
+      // reply to _message_ by using the _bot_ object
+        log.warn("New user: ");
+        log.debug(message);
+      bot.reply(message,'Ok. user_channel_join!');
+
+    });
+    // reply to a direct message
+    controller.on('direct_message',function(bot,message) {
+
+      // reply to _message_ by using the _bot_ object
+      //bot.reply(message,'You are talking directly to me');
+
+    });
+
+    bot.api.users.getPresence({},function(err,response) {
+        log.info('....api.users.getPresence...');
+        //log.info(response);
+    });
+
+    bot.api.users.info({},function(err,response) {
+        log.info('....api.users.info...');
+        //log.info(response);
+    });
+
+    bot.api.users.list({},function(err,response) {
+        log.info('....api.users.list...');
+        //log.info(response);
+    });
+
+    bot.api.users.setActive({},function(err,response) {
+        log.info('....api.users.setActive...');
+        //log.info(response);
+    });
+
+    bot.api.users.setPresence({},function(err,response) {
+        log.info('....api.users.setPresence...');
+        //log.info(response);
+    });
+
+    bot.api.channels.list({},function(err,response) {
+        log.info('...channels.list...');
+        //log.info(response); //All channels list
+    });
+
+    bot.api.channels.info({},function(err,response) {
+        log.info('...api.channels.info...');
+        //log.info(response); //All channel info
+    });
+
+    bot.api.channels.join({},function(err,response) {
+        log.info('...api.channels.join...');
+        //log.info(response); //Channel join
+    });
+
+    bot.api.channels.leave({},function(err,response) {
+        log.info('...api.channels.leave...');
+        //log.info(response); //Channel leave
+    });*/
---
+++ slackbot.js
@@ -0,0 +1,259 @@
+/*
+
+    #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+      This file is part of the Smart Developer Hub Project:
+        http://www.smartdeveloperhub.org/
+      Center for Open Middleware
+            http://www.centeropenmiddleware.com/
+    #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+      Copyright (C) 2015 Center for Open Middleware.
+    #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+      Licensed under the Apache License, Version 2.0 (the "License");
+      you may not use this file except in compliance with the License.
+      You may obtain a copy of the License at
+                http://www.apache.org/licenses/LICENSE-2.0
+      Unless required by applicable law or agreed to in writing, software
+      distributed under the License is distributed on an "AS IS" BASIS,
+      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+      See the License for the specific language governing permissions and
+     limitations under the License.
+    #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+*/
+
+    var loadStartDate = new Date();
+    try {
+        // global buyan
+        const bunyan = require('bunyan');
+        const PrettyStream = require('bunyan-prettystream');
+    } catch (err) {
+        console.error("Bot Error. bunyan logs problem: " + err);
+    }
+    try {
+        // Set Config params
+        require('./config');
+    } catch (err) {
+        console.error("Fatal BOT Error with config: " + err);
+        setTimeout(function() {
+            process.exit(0);
+        }, 1000);
+    }
+
+    // Shut down function
+    var gracefullyShuttinDown = function gracefullyShuttinDown() {
+        slog.warn('Shut down signal Received ');
+        slog.warn(" ! Shutting Down SDH-API manually.");
+        setTimeout(function () {
+            process.exit(0);
+        }, 500);
+    };
+    // Set security handlers
+    process.on('SIGINT', gracefullyShuttinDown);
+    process.on('SIGTERM', gracefullyShuttinDown);
+
+    if (!SLACK_BOT_TOKEN) {
+        slog.error('SLACK_BOT_TOKEN not found');
+        gracefullyShuttinDown();
+        return;
+    }
+
+    /* File Log */
+    var prettyStdOut = new PrettyStream();
+    prettyStdOut.pipe(process.stdout);
+    GLOBAL.slog = null;
+    GLOBAL.mkdirp = require("mkdirp");
+    var getDirName = require("path").dirname;
+    mkdirp(getDirName(FILE_LOG_PATH), function (err) {
+        if (err) {
+            console.error("! Log file disabled");
+            console.error("Error creating log file " +  FILE_LOG_PATH);
+            slog.error(err);
+        } else {
+            slog = bunyan.createLogger({
+                    name: 'SLACK-BOT',
+                    streams: [{
+                        level: CONSOLE_LOG_LEVEL,
+                        stream: prettyStdOut
+                    },
+                    {
+                        level: FILE_LOG_LEVEL,
+                        type: 'rotating-file',
+                        path: FILE_LOG_PATH,
+                        period: FILE_LOG_PERIOD + 'h',
+                        count: FILE_LOG_NFILES
+                    }]
+            });
+            startBOT();
+        }
+    });
+
+    var getSlackMembers = function getSlackMembers(callback) {
+        bot.api.users.list({}, function(err, res) {
+            if (err) {
+                log.error(err);
+                return;
+            }
+            log.info("Slack Users List:");
+            log.info(JSON.stringify(res.members)); // Array
+            callback(res.members);
+        });
+    };
+
+    var startBOT = function startBOT () {
+        slog.info("... Loading Modules...");
+        try {
+            GLOBAL.moment = require("moment");
+            GLOBAL.request = require('request');
+        } catch (err) {
+            slog.error(" ! Error loading dependencies: " + err);
+            slog.info('Exiting...');
+            setTimeout(function () {
+                process.exit(0);
+            }, 500);
+        }
+        slog.info('...starting...');
+        GLOBAL.sdhBot = require('sdh-core-bot');
+        sdhBot.setSdhApiUrl('https://sdh.conwet.fi.upm.es/sdhapi');
+        sdhBot.setSdhDashboardUrl('https://sdh.conwet.fi.upm.es/');
+        // Send the slack-member-id format relationship with sdh-member-id
+        sdhBot.init("<@USLACKBOT>", function() {
+            sdhBot.getSDHMembers(function(sdhMembers) {
+                // Launching SlackBot
+                launchSlackBot(function() {
+                    slog.info('... slackbot Listenen! ...');
+                    bot.getSlackMembers(function(slackMembers) {
+                        log.info("SDH Members List:");
+                        log.info(JSON.stringify(sdhMembers)); // Array
+                        log.info("SLACK Members List:");
+                        log.info(JSON.stringify(slackMembers)); // Array
+
+                        GLOBAL.usersBySlackId = {};
+                        GLOBAL.usersBySDHId = {};
+                        GLOBAL.usersByNick = {};
+                        // Pointers. Read Only
+                        GLOBAL.knownUsers = [];
+                        GLOBAL.knownUsersBySlackId = {};
+                        // Ids relationship for sdh-core-bot {slack-userTag-in-msg: sdhId}
+                        var idRel = {};
+
+                        for (var i = 0; i < slackMembers.length; i++) {
+                            var mi = slackMembers[i];
+                            if (mi.deleted) {
+                                continue;
+                            }
+                            var u = {
+                                slack_name: mi.name,
+                                slack_id: mi.id,
+                                slack_team_id: mi.team_id,
+                                slack_first_name: mi.profile.first_name,
+                                slack_last_name: mi.profile.last_name,
+                                slack_real_name: mi.profile.real_name_normalized,
+                                slack_avatar: mi.profile.image_192, // image_original is optional... gravatar etc
+                                slack_email: mi.profile.email,
+                                slack_color: mi.color
+                            };
+                            usersBySlackId[mi.id] = u;
+                            usersByNick[mi.name] = u;
+                        }
+                        for (var z = 0; z < sdhMembers.length; z++) {
+                            var mix = false;
+                            var mi = sdhMembers[z];
+                            // position
+                            var pos;
+                            switch (mi.positionsByOrgId[1][0]) { // By de moment only 1 org and only 1 position for each org
+                                 case 1 :
+                                    pos = 'Director';
+                                    break;
+                                case 2 :
+                                    pos = "Product Manager";
+                                    break;
+                                case 3 :
+                                    pos = "Architect";
+                                    break;
+                                case 4 :
+                                    pos = "Developer";
+                                    break;
+                                default:
+                                    pos = "Unknown";
+                                    break;
+                            };
+
+                            if (mi.nick in usersByNick) {
+                                // SDH & Slack user !! Mixing...
+                                mix = true;
+                                var mis = usersByNick[mi.nick];
+                                usersBySDHId[mi.userid] = {
+                                    slack_name: mis.slack_name,
+                                    slack_id: mi.slack_id,
+                                    slack_team_id: mis.slack_team_id,
+                                    slack_first_name: mis.slack_first_name,
+                                    slack_last_name: mis.slack_last_name,
+                                    slack_real_name: mis.slack_real_name,
+                                    slack_avatar: mis.slack_avatar,
+                                    slack_email: mis.slack_email,
+                                    slack_color: mis.color
+                                };
+                                usersBySlackId[mis.slack_id]['sdh_nick'] = mi.nick;
+                                usersBySlackId[mis.slack_id]['sdh_id'] = mi.userid;
+                                usersBySlackId[mis.slack_id]['sdh_name'] = mi.name;
+                                usersBySlackId[mis.slack_id]['sdh_avatar'] = mi.avatar;
+                                usersBySlackId[mis.slack_id]['sdh_email'] = mi.email;
+                                usersBySlackId[mis.slack_id]['sdh_position']  = pos;
+                                usersBySlackId[mis.slack_id]['sdh_register']  = mi.register;
+                            } else {
+                                // Only SDH member
+                                usersByNick[mi.nick] = {};
+                                usersBySDHId[mi.userid] = {};
+                            }
+
+                            usersBySDHId[mi.userid]['sdh_nick'] = mi.nick;
+                            usersBySDHId[mi.userid]['sdh_id'] = mi.userid;
+                            usersBySDHId[mi.userid]['sdh_name'] = mi.name;
+                            usersBySDHId[mi.userid]['sdh_avatar'] = mi.avatar;
+                            usersBySDHId[mi.userid]['sdh_email'] = mi.email;
+                            usersBySDHId[mi.userid]['sdh_position'] = pos;
+                            usersBySDHId[mi.userid]['sdh_register'] = mi.register;
+
+                            usersByNick[mi.nick]['sdh_nick'] = mi.nick;
+                            usersByNick[mi.nick]['sdh_id'] = mi.userid;
+                            usersByNick[mi.nick]['sdh_name'] = mi.name;
+                            usersByNick[mi.nick]['sdh_avatar'] = mi.avatar;
+                            usersByNick[mi.nick]['sdh_email'] = mi.email;
+                            usersByNick[mi.nick]['sdh_position'] = pos;
+                            usersByNick[mi.nick]['sdh_register'] = mi.register;
+
+                            if(mix) {
+                                // Read Only
+                                knownUsers.push(usersByNick[mi.nick]);
+                                knownUsersBySlackId[mis.slack_id] = usersByNick[mi.nick];
+                                // slack user example format in msg --> '<@u04ekpega>'
+                                idRel['<@' + mis.slack_id + '>'] = mi.userid;
+                            }
+                        }
+
+                        log.debug('-----------------  usersByNick   --------------------');
+                        log.debug(JSON.stringify(usersByNick));
+                        log.debug('-----------------  usersBySlackId   --------------------');
+                        log.debug(JSON.stringify(usersBySlackId));
+                        log.debug('-----------------  usersBySDHId   --------------------');
+                        log.debug(JSON.stringify(usersBySlackId));
+                        log.debug('-----------------  knownUsers   --------------------');
+                        log.debug(JSON.stringify(knownUsers));
+                        log.debug('-----------------  knownUsersBySlackId   --------------------');
+                        log.debug(JSON.stringify(knownUsersBySlackId));
+                        sdhBot.setMembersIdRel(idRel);
+                    });
+                });
+            });
+
+        });
+    };
+
+    GLOBAL.bot;
+    var launchSlackBot = function launchSlackBot (callback) {
+        slog.info('...loading slack bot interface ...');
+        bot = require("./slackInterface.js");
+        bot.setListeners(function() {
+            slog.info('...slack interface ready...');
+            callback();
+        });
+    };
